b8839da322a80e2f14637490919990f64700703d
ayushdhiman101 <ayushdhiman101@gmail.com>
2021-07-11 23:50:34 +0530

    Create Remove Nth Node From End of List.java

---
+++ Remove Nth Node From End of List.java
@@ -0,0 +1,22 @@
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
+        }
+        length -= n;
+        first = dummy;
+        while (length > 0) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+}
