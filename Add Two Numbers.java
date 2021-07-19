c253cebd5102b7a99c0a392235e989d722cca21d
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2021-07-20 00:17:27 +0530

    Update Add Two Numbers.java

--- Add Two Numbers.java
+++ Add Two Numbers.java
@@ -6,6 +6,7 @@
  * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
  * ListNode next) { this.val = val; this.next = next; } }
  */
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
