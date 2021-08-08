2b45063319ea16d74993dc324984e5cb41af7f31
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2021-08-08 16:49:28 +0530

    Create Best Time to Buy and Sell Stock II.java

---
+++ Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,16 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+// Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
