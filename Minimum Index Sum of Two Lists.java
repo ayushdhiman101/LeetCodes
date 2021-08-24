3a4ce8346e5118a0fd711ca707e7f32d5fe43afd
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2021-08-24 17:30:37 +0530

    Create Minimum Index Sum of Two Lists.java

---
+++ Minimum Index Sum of Two Lists.java
@@ -0,0 +1,32 @@
+// Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+
+// You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+
+
+
+public class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        
+        HashMap < String, Integer > map = new HashMap < String, Integer > ();
+        
+        for (int i = 0; i < list1.length; i++)
+            map.put(list1[i], i);
+        
+        List < String > res = new ArrayList < > ();
+        
+        int min_sum = Integer.MAX_VALUE, sum;
+        for (int j = 0; j < list2.length && j <= min_sum; j++) {
+            if (map.containsKey(list2[j])) {
+                sum = j + map.get(list2[j]);
+                if (sum < min_sum) {
+                    res.clear();
+                    res.add(list2[j]);
+                    min_sum = sum;
+                } else if (sum == min_sum)
+                    res.add(list2[j]);
+            }
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
+
