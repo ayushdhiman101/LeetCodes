b754540089dcffb64f20131b68778d6557923e6b
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2021-09-22 16:33:43 +0530

    Create Odd Even Linked List.java

---
+++ Odd Even Linked List.java
@@ -0,0 +1,59 @@
+// Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+// The first node is considered odd, and the second node is even, and so on.
+
+// Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+// You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        
+        ListNode dummy=new ListNode(-1);
+        ListNode retain=dummy;
+        ListNode p=head;
+        int count=1;
+        
+        ListNode dummy2=new ListNode(-1);
+        ListNode retain2=dummy2;
+        
+        
+        
+        while(p!=null){
+       
+        if(count%2!=0){
+            ListNode add=new ListNode(p.val);
+            dummy.next=add;
+            dummy=dummy.next;
+            
+        }
+        else{
+            ListNode add2=new ListNode(p.val);
+            dummy2.next=add2;
+            dummy2=dummy2.next;
+            
+                
+        }
+       
+        p=p.next;
+        
+        count++;
+        }
+        
+        dummy.next=retain2.next;
+        return retain.next;
+        
+    }
+}
