ab88632c152f79dd3d06facfac5f2b5e4f327c87
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-01-25 10:53:22 +0530

    Time: 2 ms (56.28%), Space: 51.8 MB (13.36%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.java
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -2,12 +2,6 @@ class Solution {
     public boolean validMountainArray(int[] arr) {
        
         if(arr.length==1) return false;
-        // if(arr.length==2){
-        //     if(arr[1]>arr[0])
-        //         return true;
-        //     else
-        //         return false;
-        // }
         
         int i=1;
         boolean flag=false;;
@@ -20,8 +14,8 @@ class Solution {
             i++;
             flag2=true;
         }
-        if(i==arr.length && flag && flag2) return true;
-        else return false;
+        return (i==arr.length && flag && flag2);
+        
         
     }
 }
