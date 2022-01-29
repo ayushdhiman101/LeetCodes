c8713479b4572ea6f747e8a26338c71c3ee10bcf
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-01-30 03:20:15 +0530

    Time: 2 ms (71.72%), Space: 54.2 MB (5.80%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.java
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -3,19 +3,19 @@ class Solution {
        
         if(arr.length==1) return false;
         
+        boolean flag=false;
+        boolean flag2=false;
+        
         int i=1;
-        boolean flag=false;;
         while(i<arr.length && arr[i]>arr[i-1]){
             i++;
             flag=true;
         }
-        boolean flag2=false;
         while(i<arr.length && arr[i]<arr[i-1] && flag){
             i++;
             flag2=true;
         }
+        
         return (i==arr.length && flag && flag2);
-        
-        
     }
 }
