8a271979cb16ee0e3383764dc35057b6edc5ea86
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-02-03 01:31:40 +0530

    Time: 887 ms (9.49%), Space: 118.1 MB (5.07%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,16 +1,39 @@
 class Solution {
+          
+
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<Integer>();
-        char arrayp[] = p.toCharArray();
-        Arrays.sort(arrayp);
-        for(int i = 0;i<=s.length()-p.length();i++){
-            char array[] = (s.substring(i,i+p.length())).toCharArray();
-            Arrays.sort(array);
-            if(Arrays.equals(array,arrayp)){
-                list.add(i);
-            }
-        }
-        return list;
+        List<Integer> list =new ArrayList<>();
         
+        int shash[]= new int[26];
+        for(int i=0;i<p.length();i++){
+            char c=p.charAt(i);
+            shash[c-'a']+=1;
+        }
+        
+        int plen=p.length();
+        int slen=s.length();
+        
+        for(int i=0;i<=slen-plen;i++){
+            int b[] = Arrays.copyOf(shash, shash.length);
+
+            String sub=s.substring(i,i+plen);
+            boolean flag=true;
+            for(int j=0;j<plen;j++){
+                int idx=sub.charAt(j)-'a';
+                if( b[idx]==0 ){
+                    flag=false;
+                    break;
+                }
+                    
+                else
+                    b[idx]-=1;
+                    
+                    
+            }
+            if(flag==true) list.add(i);
+        }
+            
+    
+        return list;   
     }
 }
