b2315dcfee7bd1aa58e83da7c1bb5ad57d73a2bb
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-02-07 14:03:48 +0530

    Time: 2 ms (75.61%), Space: 40.4 MB (18.78%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,22 +1,47 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap <Character,Integer> hash=new HashMap<>();
-        for(char i : s.toCharArray())
-            if(!(hash.containsKey(i)))
-                hash.put(i,1);
-            else 
-                hash.put(i,hash.get(i)+1);
-            
-        for(char i : t.toCharArray())
-            if(hash.containsKey(i) && hash.get(i)>0){
-                hash.put(i,hash.get(i)-1);
-            }
-            else if(hash.containsKey(i) && hash.get(i)==0){
-                return i;
-            }
-            else {
-                return i;
-            }
-        return 'N';  
+        
+        char arr1[]=s.toCharArray();
+        char arr2[]=t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        if(arr1.length==0) return arr2[0];
+        for(int i=0;i<arr1.length;i++){
+            if(arr1[i]!=arr2[i])
+                return arr2[i];
+        }
+        return arr2[arr2.length-1];
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         HashMap <Character,Integer> hash=new HashMap<>();
+//         for(char i : s.toCharArray())
+//             if(!(hash.containsKey(i)))
+//                 hash.put(i,1);
+//             else 
+//                 hash.put(i,hash.get(i)+1);
+          
+//         for(char i : t.toCharArray())
+//             if(hash.containsKey(i) && hash.get(i)>0){
+//                 hash.put(i,hash.get(i)-1);
+//             }
+//             else if(hash.containsKey(i) && hash.get(i)==0){
+//                 return i;
+//             }
+//             else {
+//                 return i;
+//             }
+//         return '';  
     }
 }
