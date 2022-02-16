587948a6e02c41a53387451e46e918989466cea2
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-02-16 17:54:17 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (30.34%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -0,0 +1,19 @@
+  class Solution {
+
+        public ListNode swapPairs(ListNode head) {
+            ListNode dummy = new ListNode(-1);
+            ListNode prev = dummy;
+            ListNode current = head;
+            dummy.next = head;
+            
+            while (current != null && current.next != null) {
+                prev.next = current.next;
+                current.next = current.next.next;
+                prev.next.next = current;
+                current = current.next;
+                prev = prev.next.next;
+            }
+
+            return dummy.next;
+        }
+    }
