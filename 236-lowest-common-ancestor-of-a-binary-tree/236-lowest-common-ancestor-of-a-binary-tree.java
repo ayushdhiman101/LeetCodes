fc39ca8caac64433054e1c95a869b9dae064c3aa
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-03-17 00:49:53 +0530

    Time: 11 ms (45.99%), Space: 46.6 MB (61.53%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -7,47 +7,104 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
-    
-    
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList <TreeNode> list = new ArrayList<>();
-        ArrayList <TreeNode> list2 = new ArrayList<>();
-        rootToNode(root,p,list);
-        rootToNode(root,q,list2);
-        print(list,list2);
-        return solve(list,list2);
-    }
-    public boolean rootToNode(TreeNode root,TreeNode p,List <TreeNode> arr){
-        if(root == null) return false; 
-        
-        arr.add(root);
-        if(root == p) return true; 
-        
-        if(rootToNode(root.left, p, arr) || rootToNode(root.right, p, arr)) return true; 
-        
-        arr.remove(arr.size() - 1); 
-        
-        return false; 
-    }
-    public void print(List <TreeNode> list,List <TreeNode> list2){
-        for(TreeNode i : list)
-            System.out.print(i.val + " ");
-        System.out.println( " ");
-        for(TreeNode i : list2)
-            System.out.print(i.val + " ");
-        
-    }
-    public TreeNode solve(List <TreeNode> list,List <TreeNode> list2) {
-        int i=0,j=0;
-        while(list.get(i)==list2.get(j)){
-            
-            if(i<list.size()-1 && j<list2.size()-1){
-                i++;j++;
-            }
-            else
-                return list.get(i);
-        }
-        return list.get(i-1);
-    }
+     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+         if(root == null || root == p || root == q)
+             return root;
+         
+         TreeNode left = lowestCommonAncestor(root.left , p , q);
+         TreeNode right = lowestCommonAncestor(root.right , p , q);
+         
+         
+         if(left== null  )
+             return right;
+         else if (right == null)
+             return left;
+         else 
+            return root;
+         
+             
+         
+     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOLUTION 1 
+
+//     TC : O(N) + O(N)
+//     SC : O(N) + O(N)
+    
+//         NOT OPTIMAL
+    
+    
+// class Solution {
+    
+    
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         ArrayList <TreeNode> list = new ArrayList<>();
+//         ArrayList <TreeNode> list2 = new ArrayList<>();
+//         rootToNode(root,p,list);
+//         rootToNode(root,q,list2);
+//         print(list,list2);
+//         return solve(list,list2);
+//     }
+//     public boolean rootToNode(TreeNode root,TreeNode p,List <TreeNode> arr){
+//         if(root == null) return false; 
+        
+//         arr.add(root);
+//         if(root == p) return true; 
+        
+//         if(rootToNode(root.left, p, arr) || rootToNode(root.right, p, arr)) return true; 
+        
+//         arr.remove(arr.size() - 1); 
+        
+//         return false; 
+//     }
+//     public void print(List <TreeNode> list,List <TreeNode> list2){
+//         for(TreeNode i : list)
+//             System.out.print(i.val + " ");
+//         System.out.println( " ");
+//         for(TreeNode i : list2)
+//             System.out.print(i.val + " ");
+        
+//     }
+//     public TreeNode solve(List <TreeNode> list,List <TreeNode> list2) {
+//         int i=0,j=0;
+//         while(list.get(i)==list2.get(j)){
+            
+//             if(i<list.size()-1 && j<list2.size()-1){
+//                 i++;j++;
+//             }
+//             else
+//                 return list.get(i);
+//         }
+//         return list.get(i-1);
+//     }
+// }
