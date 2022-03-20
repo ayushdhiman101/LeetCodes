b0f39cd31ac23be63c6302f89803b97f1d6459e9
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-03-20 15:46:49 +0530

    Time: 36 ms (90.11%), Space: 13.4 MB (46.89%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
-    bool winnerOfGame(string s) {
-        int n=s.size();
-        int cnta=0,cntb=0;
-        int acta=0,actb=0;
-        char ch=s[0];
-        if(ch=='A')cnta++;
-        else cntb++;
-        for(int i=1;i<n;i++){
-            if(ch==s[i]){
-                if(ch=='A')cnta++;
-                else cntb++;
-            }
-            else{
-                if(ch=='A'){
-                    acta+=cnta-2<0?0:cnta-2;
-                    cnta=0;
-                    cntb=1;
-                    ch=s[i];
-                }
-                else{
-                    actb+=cntb-2<0?0:cntb-2;
-                    cntb=0;
-                    cnta=1;
-                    ch=s[i];
-                }
-            }
+    bool winnerOfGame(string colors) 
+    {
+        int alice = 0,bob = 0;
+        int i = 0;
+        while(i < colors.size())
+        {
+            int countA = 0,countB = 0;
+            if(colors[i] == 'A')while(colors[i] == 'A' and i < colors.size()){countA++;i++;}
+            else while(colors[i] == 'B' and i < colors.size()){countB++;i++;}
+            if(countA > 2)alice += countA-2;
+            else if(countB > 2)bob += countB-2;
         }
-        if(ch=='A')acta+=cnta-2<0?0:cnta-2;
-        else actb+=cntb-2<0?0:cntb-2;
-        if(actb>acta){
-            return false;
-        }
-        else if(acta==actb){
-            return false;
-        }
-        return true;
+        return alice > bob;
     }
 };
