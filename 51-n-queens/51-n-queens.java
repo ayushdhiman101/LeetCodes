8bdcd3333644affadc4046b0212d5e4db3b846af
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-06-04 17:11:35 +0530

    Time: 6 ms (54.02%), Space: 45.5 MB (45.27%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,7 +1,3 @@
-// CHECK FUNCTION USED 
-// TC = O(N! * N) 
-// SC = O(N^2)
-
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
@@ -11,12 +7,17 @@ class Solution {
                 board[i][j]=".";
             }
         }
-        solve(n,ans,board,0);
+        
+        int[] leftRow= new int[n];
+        int[] lowerDiagonal= new int[2*n-1];
+        int[] upperDiagonal= new int[2*n-1];
+        
+        solve(n,ans,board,0 , leftRow,lowerDiagonal,upperDiagonal);
         return ans;
         
     }
     
-     public void solve(int n, List<List<String>> ans, String [][] board,int col) {
+     public void solve(int n, List<List<String>> ans, String [][] board,int col ,int[]leftRow, int[]lowerDiagonal, int[]upperDiagonal) {
          if(col==n){
             List<String> list =new ArrayList<>(convert(board,n));
             ans.add(list);
@@ -24,41 +25,24 @@ class Solution {
         }
         
          for(int row=0;row<n;row++){
-             if(check(row,col,board,n)){
-                board[row][col]="Q";
-                solve(n,ans,board,col+1);
-                board[row][col]=".";
+             if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0){
+                 
+                 board[row][col]="Q";
+                 leftRow[row]=1;
+                 lowerDiagonal[row+col]=1;
+                 upperDiagonal[n-1+col-row]=1;
+                     
+                 solve(n,ans,board,col+1,leftRow,lowerDiagonal,upperDiagonal);
+                 
+                 board[row][col]=".";
+                 leftRow[row]=0;
+                 lowerDiagonal[row+col]=0;
+                 upperDiagonal[n-1+col-row]=0;
+                 
               }
          }
     }
-    public boolean check(int row,int col, String [][] board ,int n) {
-        int duprow=row,dupcol=col;
-        
-        while(row>=0 && col>=0){
-            
-            if(board[row][col]=="Q") return false;
-            row--;
-            col--;
-        }
-        
-        row=duprow;
-        col=dupcol;
-        
-        while(col>=0){
-            if(board[row][col]=="Q") return false;
-            col--;
-        }
-        row=duprow;
-        col=dupcol;   
-        
-        while(row<n && col>=0){
-            if(board[row][col]=="Q") return false;
-            row++;
-            col--;
-        }
-        return true;
-        
-    }
+
     public List<String> convert(String [][] board ,int n) {
         ArrayList<String> list = new ArrayList<>();
         for(int i=0;i<n;i++){
@@ -72,68 +56,3 @@ class Solution {
         return list;
     }
 }
-
-
-// OPTIMIZED CODE (CHECK FUNCTION REMOVED AND HASHING UTILIZED)
-// TC = O(N! * N) 
-// SC = O(N)
-
-
-// class Solution {
-//     public List<List<String>> solveNQueens(int n) {
-//         List<List<String>> ans = new ArrayList<>();
-//        String [][] board = new String [n][n];
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<n;j++){
-//                 board[i][j]=".";
-//             }
-//         }
-        
-//         int[] leftRow= new int[n];
-//         int[] lowerDiagonal= new int[2*n-1];
-//         int[] upperDiagonal= new int[2*n-1];
-        
-//         solve(n,ans,board,0 , leftRow,lowerDiagonal,upperDiagonal);
-//         return ans;
-        
-//     }
-    
-//      public void solve(int n, List<List<String>> ans, String [][] board,int col ,int[]leftRow, int[]lowerDiagonal, int[]upperDiagonal) {
-//          if(col==n){
-//             List<String> list =new ArrayList<>(convert(board,n));
-//             ans.add(list);
-//             return;
-//         }
-        
-//          for(int row=0;row<n;row++){
-//              if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0){
-                 
-//                  board[row][col]="Q";
-//                  leftRow[row]=1;
-//                  lowerDiagonal[row+col]=1;
-//                  upperDiagonal[n-1+col-row]=1;
-                     
-//                  solve(n,ans,board,col+1,leftRow,lowerDiagonal,upperDiagonal);
-                 
-//                  board[row][col]=".";
-//                  leftRow[row]=0;
-//                  lowerDiagonal[row+col]=0;
-//                  upperDiagonal[n-1+col-row]=0;
-                 
-//               }
-//          }
-//     }
-
-//     public List<String> convert(String [][] board ,int n) {
-//         ArrayList<String> list = new ArrayList<>();
-//         for(int i=0;i<n;i++){
-//             String [] a = board[i];
-//             StringBuffer sb = new StringBuffer();
-//             for(int j = 0; j < a.length; j++) 
-//                 sb.append(a[j]);
-                  
-//             list.add(sb.toString());
-//         }
-//         return list;
-//     }
-// }
