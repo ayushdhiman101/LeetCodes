28ba5edee1b3e079dfff2d26bc5bf50059ace4c1
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-06-05 23:20:32 +0530

    Time: 1 ms (97.89%), Space: 41.8 MB (26.91%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,63 @@
+class Solution {
+    int count=0;
+    public int totalNQueens(int n) {
+        int board[][] = new int[n][n];
+        int left[]=new int[n];
+        int leftUpper[]=new int[2*n-1];
+        int leftLower[]=new int[2*n-1];
+        
+        solve(n,board,0,left,leftUpper,leftLower);
+        return count;
+    }
+    public void solve(int n,int board[][],int col,int left[],int leftUpper[],int leftLower[]){
+        if(col==n){
+            count++;
+            return;
+        }
+        for(int i=0;i<n;i++){
+                if(left[i]==0 && leftLower[i+col]==0 && leftUpper[n-1+col-i]==0){
+                    left[i]=1;
+                    leftLower[i+col]=1;
+                    leftUpper[n-1+col-i]=1;
+                    board[i][col]=1;
+                    
+                    solve(n,board,col+1,left,leftUpper,leftLower);
+                    
+                    left[i]=0;
+                    leftLower[i+col]=0;
+                    leftUpper[n-1+col-i]=0;
+                    board[i][col]=0;
+                
+            }
+        }
+        
+    }
+//      public boolean check(int n,int board[][],int row,int col){
+//          int duprow=row,dupcol=col;
+         
+//          while(row>=0 && col>=0 && row<n && col<n){
+//              if(board[row][col]==1) return false;
+//              row++;
+//              col--;
+//          }
+         
+//          row=duprow;
+//          col=dupcol;
+         
+//          while(row>=0 && col>=0 && row<n && col<n){
+//              if(board[row][col]==1) return false;
+//              row--;
+//              col--;
+//          }
+//          row=duprow;
+//          col=dupcol;
+         
+//          while(row>=0 && col>=0 && row<n && col<n){
+//              if(board[row][col]==1) return false;
+//              col--;
+//          }
+//     return true;
+//      }
+     
+    
+}
