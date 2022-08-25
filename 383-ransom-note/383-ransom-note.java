f7fba6d6f54763de2f014352de22278e592be493
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-08-25 19:58:44 +0530

    Time: 5 ms (74.47%), Space: 45.1 MB (84.56%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int arr[] = new int[26];
         for(int i=0;i<magazine.length();i++){
-            arr[(int)magazine.charAt(i)-(int)'a']+=1;
+            arr[magazine.charAt(i)-'a']+=1;
         }
          for(int i=0;i<ransomNote.length();i++){
-             if(arr[(int)ransomNote.charAt(i)-(int)'a']!=0)
-                   arr[(int)ransomNote.charAt(i)-(int)'a']-=1;
+             if(arr[ransomNote.charAt(i)-'a']!=0)
+                   arr[ransomNote.charAt(i)-'a']-=1;
              else
                  return false;
         }
