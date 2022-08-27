3a1ddf650816cf54113563dae8c07f78566d55f9
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2022-08-27 20:02:13 +0530

    Time: 2 ms (12.58%), Space: 43.3 MB (5.74%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,6 +1,16 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
-        
+        TreeMap<Integer, Integer> map= new TreeMap<Integer, Integer>();
+        for(int i=0;i<nums.length;i++)
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        int j=0;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            int x=map.get(entry.getKey());
+            int temp=entry.getKey();
+            for(int i=0;i<x;i++){
+                nums[j]=temp;
+                j++;
+            }
+        }
     }
 }
