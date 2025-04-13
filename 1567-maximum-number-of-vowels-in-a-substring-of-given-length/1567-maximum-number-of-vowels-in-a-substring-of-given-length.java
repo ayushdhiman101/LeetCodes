3b176f1108a1fc72a0e65479f918c82c6b21cf2a
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2025-04-13 16:26:21 +0530

    Time: 83 ms (5.32%), Space: 44.5 MB (93.39%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -2,15 +2,17 @@ class Solution {
     public int maxVowels(String s, int k) {
         StringBuilder str = new StringBuilder();
         int max = 0;
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+
         for(int i = 0; i < k && i < s.length(); i++) {
             str.append(s.charAt(i));
-            max += isVowel(s.charAt(i)) ? 1: 0;
+            max += vowels.contains(s.charAt(i)) ? 1: 0;
         }
         int count = max;
         for(int i = k; i < s.length(); i++) {
-            boolean addition = isVowel(s.charAt(i));
+            boolean addition = vowels.contains(s.charAt(i));
             str.append(s.charAt(i));
-            boolean deletion = isVowel(str.charAt(0));
+            boolean deletion = vowels.contains(str.charAt(0));
             str.deleteCharAt(0);
 
             if(addition == true && deletion == false ) count++;
@@ -19,11 +21,5 @@ class Solution {
             max = Math.max(count, max);
             }
         return max;
-    }
-    public boolean isVowel ( Character letter ) {
-        int count = 0;
-        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
-            return true;
-        return false;
     }
 }
