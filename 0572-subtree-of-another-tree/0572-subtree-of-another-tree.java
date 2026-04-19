c166dc34442baa4302d9a03cd615e478889a07b4
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2026-04-19 14:03:32 +0530

    Time: 3 ms (67.8%), Space: 46.8 MB (10.34%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,22 +15,17 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null || subRoot == null) 
-            return root == subRoot;
-        if(root.val == subRoot.val && isIdentical(root, subRoot)) return true;
-            
-        boolean left = isSubtree(root.left, subRoot);
-        boolean right = isSubtree(root.right, subRoot);
-
+        if(root == null || subRoot == null) return root == subRoot;
+        if(root.val == subRoot.val && isIdentical(root, subRoot)) return true; 
+        boolean left = isSubtree (root.left, subRoot);
+        boolean right = isSubtree (root.right, subRoot);
         return left || right;
-    }
-    public boolean isIdentical(TreeNode root, TreeNode subRoot) {
-        if(root == null || subRoot == null) 
-            return root == subRoot;
-            
-        boolean left = isIdentical(root.left, subRoot.left);
-        boolean right = isIdentical(root.right, subRoot.right);
 
-        return left && right && root.val == subRoot.val;
+    }
+    public boolean isIdentical(TreeNode p, TreeNode q) {
+        if(p == null || q == null) return p == q;
+        boolean left = isIdentical (p.left, q.left);
+        boolean right = isIdentical (p.right, q.right);
+        return left && right && p.val == q.val;
     }
 }
