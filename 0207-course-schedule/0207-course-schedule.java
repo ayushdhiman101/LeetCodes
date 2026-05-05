77ad52aefeceb4ee53d4a5b07b8f1f4a8c11bd76
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2026-05-06 00:10:32 +0530

    Time: 30 ms (5.02%), Space: 47 MB (77.18%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        boolean [] visited = new boolean[numCourses];
+        boolean [] recPath = new boolean[numCourses];
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i]) {
+                if(dfs(numCourses, prerequisites, i, visited, recPath))
+                    return !true;
+            }
+        }
+        return !false;
+    }
+    // u ---> v
+    public boolean dfs(int numCourses, int[][] prerequisites, int u, boolean [] visited, boolean [] recPath ) {
+        visited[u] = true;
+        recPath[u] = true;
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < prerequisites.length; i++) {
+            if(prerequisites[i][0] == u)
+                list.add(prerequisites[i][1]);
+        }
+
+        for(int v: list) {
+            if(!visited[v]) {
+                if(dfs(numCourses, prerequisites, v, visited, recPath)) {
+                    return true;
+                }
+            }
+            else if (recPath[v]) {
+                return true;
+            }
+        }
+        recPath[u] = false;
+        return false;
+    }
+}
+
+
+/*
+visited ----> in recPath = cycle 
+*/
+
