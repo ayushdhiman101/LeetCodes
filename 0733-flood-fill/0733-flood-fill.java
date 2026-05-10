1e1096da7673a5c2715fdb9d1b92aa53175cced8
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2026-05-10 17:56:36 +0530

    Time: 1 ms (36.68%), Space: 46.9 MB (24.39%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int i, int j, int color) {
+        int n = image.length;
+        int m = image[0].length;
+        int[][] ans = new int[n][m];
+        for (int k = 0; k < n; k++) {
+            ans[k] = image[k].clone(); 
+        }        
+        boolean visited [][] = new boolean[n][m];
+
+        int startColor =  image[i][j];
+
+        dfs(image, i, j, color, ans, visited, n, m, startColor);
+        return ans;
+    }
+
+    public void dfs(int[][] image, int i, int j, int color, int ans [][], boolean [][] visited, int n, int m, int startColor) {
+        if( i < 0 || j < 0 || i >= n || j >= m || image[i][j] != startColor ||  visited[i][j] == true)
+            return;
+
+        ans[i][j] = color;
+        visited[i][j] = true; 
+
+        if(i - 1 >= 0  && visited[i - 1][j] == false) {
+            dfs(image, i - 1, j, color, ans, visited, n, m, startColor);
+        } 
+        if(j - 1 >= 0 && visited[i][j - 1] == false) {
+            dfs(image, i, j - 1, color, ans, visited, n, m, startColor);
+        } 
+        if(i + 1 < n && visited[i + 1][j] == false) {
+            dfs(image, i + 1, j, color, ans, visited, n, m, startColor);
+        } 
+        if(j + 1 < m && visited[i][j + 1] == false) {
+            dfs(image, i, j + 1, color, ans, visited, n, m, startColor);
+        } 
+        return;
+    }
+}
